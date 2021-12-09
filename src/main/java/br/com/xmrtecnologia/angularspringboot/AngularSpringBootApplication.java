32635330ac2db8a35bbe0e3397e472c4e861dd39
marconunesrj <marconunes.rj@hotmail.com>
2021-12-09 15:58:33 -0300

    Primeiro Commit

---
+++ src/main/java/br/com/xmrtecnologia/angularspringboot/AngularSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.com.xmrtecnologia.angularspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularSpringBootApplication.class, args);
+	}
+
+}
